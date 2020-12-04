2195f4758c14b4215d0dd10a4c0ea3c30566259e
Jake Meyers <jakejake92@yahoo.com>
2020-12-03 17:31:30 -0700

    more project setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Header />
+		</div>
+	);
 }
 
 export default App;
