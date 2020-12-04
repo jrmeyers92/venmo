3c0dc1eb98bb74d606edcc18cd5fcb9c02354b59
Jake Meyers <jakejake92@yahoo.com>
2020-12-03 20:26:57 -0700

    got vinmo icon working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+import theme from "./Theme";
+
+const useStyles = makeStyles((theme) => ({
+	h1: {
+		fontFamily: "Sansita Swashed",
+		letterSpacing: "1px",
+		fontSize: "2.5rem",
+	},
+}));
 
 const Header = () => {
-	return <React.Fragment>hi</React.Fragment>;
+	const classes = useStyles();
+
+	return (
+		<React.Fragment>
+			<AppBar>
+				<Toolbar>
+					<h1 className={classes.h1}>vinmo</h1>
+				</Toolbar>
+			</AppBar>
+		</React.Fragment>
+	);
 };
 
 export default Header;
--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,1 +1,33 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const arcBlue = "#049309";
+const arcOrange = "#FFbA60";
+
+export default createMuiTheme({
+	palette: {
+		common: {
+			blue: `${arcBlue}`,
+			orange: `${arcOrange}`,
+		},
+		primary: {
+			main: `${arcBlue}`,
+		},
+		secondary: {
+			main: `${arcOrange}`,
+		},
+	},
+	typography: {
+		tab: {
+			fontFamily: "Raleway",
+			textTransform: "none",
+			fontWeight: 700,
+			fontSize: "1rem",
+		},
+		estimate: {
+			fontFamily: "Pacifico",
+			fontSize: "1rem",
+			textTransform: "none",
+			color: "white",
+		},
+	},
+});
